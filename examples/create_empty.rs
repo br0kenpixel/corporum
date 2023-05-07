327369978985dbddb2f83f72b323b96f70f1e8fc
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2023-05-07 19:28:38 +0200

    add random data

--- examples/create_empty.rs
+++ examples/create_empty.rs
@@ -5,7 +5,13 @@ fn main() {
     let path = PathBuf::from("test.corp");
     {
         let mut corp = Corporeum::new(&path);
-        corp.corpus_mut().add_metadata("hello");
+        let mut doc = corp.corpus_mut().create_doc();
+        let mut sent = doc.create_sentence("en");
+        let tok = sent.create_token("hello");
+        sent.add_token(tok);
+        doc.add_sentence(sent);
+        corp.corpus_mut().add_doc(doc);
+
         corp.save().unwrap();
     }
 
